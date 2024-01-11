38b233d8a5c29df5f31464b99454e70e22356b95
Sally Hu <sally.hu@kellogg.northwestern.edu>
2024-01-11 03:20:16 +0000

    Lab 4 - JOIN

--- demo.sql
+++ demo.sql
@@ -1,3 +1,18 @@
 -- Get the first and last name of every player in the database.
 
-SELECT first_name, last_name FROM players;
+-- sqlite3 baseball.sqlite3
+-- .schema
+-- .mode columns
+-- .headers on
+-- .read ____
+-- .quit
+
+-- SELECT __ FROM ____
+-- INNER JOIN ___ ON __
+-- WHERE 
+-- AND
+-- GROUP BY___
+-- ORDER BY___
+-- LIMIT ___;
+
+-- COUNT(*), AVG(column), SUM(column), MIN(column), MAX(column)
--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,4 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
+-- SELECT * FROM players LIMIT 1
+
+-- SELECT * FROM players
+SELECT SUM(stats.hits) FROM players
+INNER JOIN stats ON players.id = stats.player_id
+WHERE players.first_name = "Barry"
+AND players.last_name = "Bonds";
 
 -- Expected result:
 -- 2935
